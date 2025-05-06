eba5dee15c5aeb94d1638c5fd92470842cb0b524
hjkim0905 <eax9952@gmail.com>
2025-05-06 11:17:38 +0900

    [Feat] : Curriculm Layout Done

--- src/pages/Curriculum/components/styled.ts
+++ src/pages/Curriculum/components/styled.ts
@@ -1,9 +1,45 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 100%;
-  height: 100%;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+    width: min(calc(100% - 14rem), 984.49px);
+    height: calc(100vh - 5.5vh - 2rem);
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    margin-left: 14rem;
+    padding-top: 5.5vh;
+    box-sizing: border-box;
+    overflow: hidden;
+`;
+
+export const CurriculumBox = styled.div`
+    width: 100%;
+    height: 100%;
+    background-color: rgba(34, 34, 46, 1);
+    border-radius: 0.9375rem;
+    display: flex;
+    flex-direction: column;
+    padding: 1.3rem 1.375rem;
+`;
+
+export const Title = styled.h1`
+    color: #ffffff;
+    font-family: 'Inter', sans-serif;
+    font-weight: 700;
+    font-size: 1.25rem;
+    line-height: 1.5rem;
+    text-align: left;
+    width: 100%;
+    margin-bottom: 1.57rem;
+`;
+
+export const ChartImage = styled.img`
+    width: min(65vw, 907.81px);
+    height: min(45vh, 612.21px);
+    border-radius: 0.875rem;
+    transform: scale(1.04, 1.07);
+    transform-origin: center;
+    margin: 0 auto;
+    flex: 1;
+    object-fit: contain;
 `;
--- src/pages/Curriculum/index.tsx
+++ src/pages/Curriculum/index.tsx
@@ -2,5 +2,12 @@ import React from 'react';
 import * as S from './components/styled';
 
 export default function Curriculum() {
-    return <S.Container>이수체계도</S.Container>;
+    return (
+        <S.Container>
+            <S.CurriculumBox>
+                <S.Title>이수체계도 확인하기</S.Title>
+                <S.ChartImage src="/curriculumchart.png" alt="이수체계도 차트" />
+            </S.CurriculumBox>
+        </S.Container>
+    );
 }

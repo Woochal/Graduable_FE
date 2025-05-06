f27f7e7b9e6748b50ad2022077205ce45bc7ced6
hjkim0905 <eax9952@gmail.com>
2025-05-06 10:41:23 +0900

    [Feat] : index.tsx function name changed

--- src/pages/Curriculum/index.tsx
+++ src/pages/Curriculum/index.tsx
@@ -1,10 +1,6 @@
-import React from "react";
-import * as S from "./components/styled";
+import React from 'react';
+import * as S from './components/styled';
 
-export default function MenuBar() {
-	return (
-		<S.Container>
-			이수체계도
-		</S.Container>
-	);
+export default function Curriculum() {
+    return <S.Container>이수체계도</S.Container>;
 }

379cd67ce1021cdaf070f47fab493e42b30e60d0
hjkim0905 <eax9952@gmail.com>
2025-05-13 18:25:04 +0900

    [Chore] : Highlighted types&subject deleted

--- src/pages/Curriculum/index.tsx
+++ src/pages/Curriculum/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import * as S from './components/styled';
-import { SUBJECTS } from './types';
 
 export default function Curriculum() {
     return (
@@ -9,9 +8,6 @@ export default function Curriculum() {
                 <S.Title>이수체계도 확인하기</S.Title>
                 <S.ChartContainer>
                     <S.ChartImage src="/curriculumchart.png" alt="이수체계도 차트" />
-                    {SUBJECTS.map((subject) => (
-                        <S.SubjectHighlight key={subject.id} x={subject.position.x} y={subject.position.y} />
-                    ))}
                 </S.ChartContainer>
             </S.CurriculumBox>
         </S.Container>

01fde0ac57a13707ea9904b4934be5a80d36ef65
hjkim0905 <eax9952@gmail.com>
2025-05-13 18:25:04 +0900

    [Chore] : Highlighted types&subject deleted

--- src/pages/Curriculum/components/styled.ts
+++ src/pages/Curriculum/components/styled.ts
@@ -1,32 +1,5 @@
 import styled, { keyframes } from 'styled-components';
 
-const glowAnimation = keyframes`
-    0% {
-        box-shadow: 
-            0 0 5px rgba(74, 144, 226, 0.7),
-            0 0 10px rgba(74, 144, 226, 0.7),
-            0 0 15px rgba(74, 144, 226, 0.7),
-            inset 0 0 5px rgba(74, 144, 226, 0.7);
-        opacity: 0.8;
-    }
-    50% {
-        box-shadow: 
-            0 0 10px rgba(74, 144, 226, 0.4),
-            0 0 20px rgba(74, 144, 226, 0.4),
-            0 0 30px rgba(74, 144, 226, 0.4),
-            inset 0 0 10px rgba(74, 144, 226, 0.4);
-        opacity: 0.5;
-    }
-    100% {
-        box-shadow: 
-            0 0 5px rgba(74, 144, 226, 0.7),
-            0 0 10px rgba(74, 144, 226, 0.7),
-            0 0 15px rgba(74, 144, 226, 0.7),
-            inset 0 0 5px rgba(74, 144, 226, 0.7);
-        opacity: 0.8;
-    }
-`;
-
 export const Container = styled.div`
     width: 100%;
     height: calc(100vh - 5.5vh - 2rem);
@@ -75,19 +48,4 @@ export const ChartImage = styled.img`
     transform: scale(1.04, 1.07);
     transform-origin: center;
     object-fit: contain;
-`;
-
-export const SubjectHighlight = styled.div<{ x: number; y: number }>`
-    position: absolute;
-    left: ${(props) => props.x}px;
-    top: ${(props) => props.y}px;
-    width: 60px;
-    height: 40px;
-    pointer-events: none;
-    z-index: 2;
-    background: rgba(74, 144, 226, 0.05);
-    border-radius: 12px;
-    animation: ${glowAnimation} 2s infinite;
-    transform: translate(-50%, -50%);
-    backdrop-filter: blur(1px);
 `;

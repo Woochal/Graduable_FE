6b1f82c0ebd493f90f7298fa146da1eced737c15
Woochal <parkwoochal@gmail.com>
2025-05-18 08:03:39 +0900

    fix : 메뉴바 UI 업데이트

--- src/components/common/Menubar/components/styled.ts
+++ src/components/common/Menubar/components/styled.ts
@@ -14,17 +14,18 @@ export const BarContainer = styled.div`
   width: 100%;
   height: 100%;
   flex-direction: column;
+  justify-content: space-between;
   align-items: center;
-  padding: 0 20px;
+  padding: 0 1.563vw 3.125vw 1.563vw;
   background-color: ${(props) => props.theme.color.bgDefault};
   border-right: 1px solid ${(props) => props.theme.color.borderDefault};
 `;
 
 export const MenuContainer = styled.div`
   width: 200px;
-  height: 450px;
+  height: 520px;
   flex-direction: column;
-  gap: 9px;
+  gap: 1.563vw;
   margin-top: 100px;
 
   /* border: 1px solid red; */
@@ -43,7 +44,37 @@ export const MenuItem = styled.div<MenuItemActiveType>`
   cursor: pointer;
   
   &:hover {
-    background-color: ${(props) => props.theme.color.primary};
-    color: ${(props) => props.theme.color.textSelected};
+    background-color: ${(props) => props.isActive ? props.theme.color.primary : props.theme.color.bgCard};
+    color: ${(props) => props.isActive ? props.theme.color.textSelected : props.theme.color.textSub};
   }
 `;
+
+export const UserInfo = styled.div`
+  width: 100%;
+  height: 3.125vw;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Logo = styled.div`
+  width: 4.375vw;
+  height: 100%;
+  align-items: center;
+  margin-right: 1.875vw;
+  /* border: 1px solid red; */
+`;
+
+export const LogoImage = styled.img`
+  width: 4.375vw;
+  /* height: 100%; */
+  object-fit: cover;
+`;
+
+export const UserName = styled.div`
+  width: 4.375vw;
+  height: 100%;
+  align-items: center;
+  font-size: ${(props) => props.theme.typography.title.fontSize};
+  font-weight: ${(props) => props.theme.typography.title.fontWeight};
+`;
+

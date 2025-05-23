536f577941bfaf08295a49b0ab6d97e1ad29792e
hjkim0905 <eax9952@gmail.com>
2025-05-23 16:41:48 +0900

    [Fix] : png path changed

--- src/pages/CourseHandbook/index.tsx
+++ src/pages/CourseHandbook/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, HandbookBox, Title, Button, PDFContainer } from './components/styled';
+import courseHandbook from '../../assets/coursehandbook.pdf';
 
 const CourseHandbook = () => {
     return (
@@ -8,7 +9,7 @@ const CourseHandbook = () => {
                 <Title>현 학기 (25-1) 수강편람 확인하기</Title>
                 <PDFContainer>
                     <embed
-                        src="/coursehandbook.pdf#toolbar=0&navpanes=0"
+                        src={courseHandbook + '#toolbar=0&navpanes=0'}
                         type="application/pdf"
                         width="100%"
                         height="100%"
